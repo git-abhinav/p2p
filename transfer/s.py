a4e175f220ef31868466af785a62d4a30993df2e
Abhinav Kumar <abhinavk.git@gmail.com>
2019-02-01 22:56:04 +0530

    server code changes

--- transfer/s.py
+++ transfer/s.py
@@ -1,23 +1,41 @@
-import socket                   
-port = 50000                    
-s = socket.socket()             
-host = "192.168.0.4"   
-s.bind((host, port))            
-s.listen(5)                     
-print ('S')
-while True:
-    conn, addr = s.accept()     
-    print ('Got connection from', addr)
-    data = conn.recv(1024)
-    print('Server received', repr(data))
-    filename='i.jpg' 
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
-    print('Done sending')
-    conn.send('Thanks')
-    conn.close()
+import socket # for connection 
+import sys    # for commands 
+def createSocket():
+    global host
+    global port 
+    global s
+    host = ""
+    port = 9999
+    try:
+        s = socket.socket()
+    except socket.error as message:
+        print("Error while creating socket"+str(message))
+
+# binding the socket and listening for connections 
+def bindSocket():
+    global host # accessing the global varibles of another function(createSocket)
+    global port 
+    global s
+    print("Binding the port : "+str(port))
+    try:
+        s.bind((host,  port ))
+        s.listen(5) # listen for connections
+                    # 5 indicates max connection s
+                    # it can tollerate 
+    except socket.error as message:
+        print("Error while creating socket "+str(message)+" trying again")
+        bindSocket()
+
+# establish connection with the client, socket must be listening 
+def socketAccept():
+    conn, address = s.accep()
+    # for accepting the connection 
+    # and it also gives two things in return 
+    # 1. object of connection 
+    # 2. list ip and and port 
+    print("Connetion successful!", address)
+    conn.close() # for closing the connection
+
+
+
+
